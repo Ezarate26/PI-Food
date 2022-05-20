0f19670cfc5a4bb33a9e3667f92eff4432c4a0f3
Emmanuel Luis Zarate <ezarate2699@gmail.com>
2022-05-20 14:32:02 -0500

    Styles components in progress

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,14 @@ import "./App.css";
 import FoodG from "./components/FoodG/FoodG.jsx";
 import FoodDetail from "./components/FoodDetail/FoodDetail.jsx";
 import NewRecipe from "./components/NewRecipe/NewRecipe";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
+      <Route exact path="/">
+        <LandingPage />
+      </Route>
       <Route exact path="/recipes">
         <FoodG />
       </Route>
---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import imagen from "../../assets/salad-2068220_1920.jpg";
+import video from "../../assets/Sizzling.mp4";
+import styles from "./LandingPage.module.css";
+
+const LandingPage = () => {
+  return (
+    <div className={styles.landing}>
+      <video
+        className={styles.videoc}
+        src={video}
+        type="video/mp4"
+        autoplay="true"
+        muted="true"
+        loop="true"
+      ></video>
+      <div className={styles.overlay}> </div>
+      <div>
+        <h1 className={styles.title}>Happy Cooking!</h1>
+        <div className={styles.buttonContainer}>
+          <img
+            className={styles.imageCard}
+            src={imagen}
+            alt="imagen de recetas"
+          />
+          <p>The best recipes</p>
+          <Link className={styles.button} to="/recipes">
+            Welcome
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
